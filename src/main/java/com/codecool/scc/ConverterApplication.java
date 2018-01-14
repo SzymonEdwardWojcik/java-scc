e17ddbe6778dc3a6da6f8eafaf1ad72cba4d8f2a
wutismyname <plebejoosh@gmail.com>
2018-01-14 19:56:40 +0100

    Add 0 args condition handling

--- src/main/java/com/codecool/scc/ConverterApplication.java
+++ src/main/java/com/codecool/scc/ConverterApplication.java
@@ -4,5 +4,9 @@ public class ConverterApplication {
 
     public static void main(String[] args) {
 
+        if (args.length < 1) {
+            System.out.println("No input file defined");
+        }
+
     }
 }
