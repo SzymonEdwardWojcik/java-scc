b87b1fc6a8c598ba65216d81b6b97eaa41f499b8
wutismyname <plebejoosh@gmail.com>
2018-01-14 21:13:11 +0100

    Add switch to main

--- src/main/java/com/codecool/scc/ConverterApplication.java
+++ src/main/java/com/codecool/scc/ConverterApplication.java
@@ -1,11 +1,29 @@
 package com.codecool.scc;
 
+import java.io.File;
+
 public class ConverterApplication {
 
     public static void main(String[] args) {
 
-        if (args.length < 1) {
-            System.out.println("No input file defined");
+        File file;
+
+        switch (args.length) {
+
+            case 0:
+                System.out.println("No input file defined");
+                break;
+
+            case 1:
+                file = new File(args[0]);
+                break;
+
+            case 2:
+                file = new File(args[1]);
+                break;
+
+            default:
+                System.out.println("Number of arguments should be 1 or 2");
         }
 
     }
