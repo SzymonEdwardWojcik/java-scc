c6b1407c132211a620f9e8c8164009a435ad1d55
wutismyname <plebejoosh@gmail.com>
2018-01-14 22:50:46 +0100

    Aggregate FileReader into converter class

--- src/main/java/com/codecool/scc/services/SimpleCsvConverter.java
+++ src/main/java/com/codecool/scc/services/SimpleCsvConverter.java
@@ -3,8 +3,15 @@ package com.codecool.scc.services;
 import com.codecool.scc.view.OutputFormat;
 
 import java.io.File;
+import java.io.FileReader;
 
 public class SimpleCsvConverter {
+
+    private FileReader fr;
+
+    public SimpleCsvConverter(FileReader fr) {
+        this.fr = fr;
+    }
 
     public void convert(File file, OutputFormat outputFormat) {
         System.out.println("I convert CSV to output format");
