d9f75c800f093f5d3bd7dff14489e82a01ee9a69
wutismyname <plebejoosh@gmail.com>
2018-01-14 22:15:32 +0100

    Add calling convert methods in main

--- src/main/java/com/codecool/scc/ConverterApplication.java
+++ src/main/java/com/codecool/scc/ConverterApplication.java
@@ -1,11 +1,16 @@
 package com.codecool.scc;
 
+import com.codecool.scc.services.SimpleCsvConverter;
+import com.codecool.scc.view.OutputFormat;
+
 import java.io.File;
 
 public class ConverterApplication {
 
     public static void main(String[] args) {
 
+        SimpleCsvConverter scc = new SimpleCsvConverter();
+        OutputFormat outputFormat;
         File file;
 
         switch (args.length) {
@@ -16,14 +21,22 @@ public class ConverterApplication {
 
             case 1:
                 file = new File(args[0]);
+                scc.convert(file);
                 break;
 
             case 2:
+                try {
+                    outputFormat = OutputFormat.valueOf(args[0].toUpperCase());
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Invalid format type. Enter json, xml or table");
+                    break;
+                }
                 file = new File(args[1]);
+                scc.convert(file, outputFormat);
                 break;
 
             default:
-                System.out.println("Number of arguments should be 1 or 2");
+                System.out.println("Invalid number of arguments. Enter only one or two arguments");
         }
 
     }
