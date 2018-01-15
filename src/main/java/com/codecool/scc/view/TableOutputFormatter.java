3603c5543320e1261d18d646e4da4e12f2fdac2f
wutismyname <plebejoosh@gmail.com>
2018-01-15 09:12:54 +0100

    Implement PrettyTable in table output formatter

--- src/main/java/com/codecool/scc/view/TableOutputFormatter.java
+++ src/main/java/com/codecool/scc/view/TableOutputFormatter.java
@@ -1,11 +1,18 @@
 package com.codecool.scc.view;
 
+import org.sk.PrettyTable;
+
 import java.util.List;
 
 public class TableOutputFormatter implements OutputFormatter {
 
     @Override
     public void printToConsole(List<String[]> data) {
-        System.out.println("printedTableHere!");
+        PrettyTable prettyTable = new PrettyTable(data.get(0));
+
+        for (int i = 0; i < data.size(); i++) {
+            prettyTable.addRow(data.get(i));
+        }
+        System.out.println(prettyTable);
     }
 }
