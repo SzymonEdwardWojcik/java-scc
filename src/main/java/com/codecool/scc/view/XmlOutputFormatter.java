3290b1023c28c48c4cb907292ca76c5444ff0f49
wutismyname <plebejoosh@gmail.com>
2018-01-15 09:16:55 +0100

    Implement XML converter

--- src/main/java/com/codecool/scc/view/XmlOutputFormatter.java
+++ src/main/java/com/codecool/scc/view/XmlOutputFormatter.java
@@ -6,6 +6,16 @@ public class XmlOutputFormatter implements OutputFormatter {
 
     @Override
     public void printToConsole(List<String[]> data) {
-        System.out.println("printedXmlHere!");
+        System.out.println("<csv>");
+
+        for (String[] line : data) {
+            System.out.println("\t<row>");
+
+            for (String column : line) {
+                System.out.println("\t\t<column>" + column + "</column>");
+            }
+            System.out.println("\t</row>");
+        }
+        System.out.println("</csv>");
     }
 }
