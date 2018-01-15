19ca787816d5b94a9841640dbd4dec78aa2602b3
wutismyname <plebejoosh@gmail.com>
2018-01-16 00:12:37 +0100

    Mark as component

--- src/main/java/com/codecool/scc/dao/FileReader.java
+++ src/main/java/com/codecool/scc/dao/FileReader.java
@@ -1,11 +1,13 @@
 package com.codecool.scc.dao;
 
 import com.opencsv.CSVReader;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+@Component
 public class FileReader {
 
     public List<String[]> readData(File file) throws IOException {
