4fd961a2e5ee3ee516e5b01842f38a1cfca34106
wutismyname <plebejoosh@gmail.com>
2018-01-15 09:26:55 +0100

    Implement convert method with only file parameter

--- src/main/java/com/codecool/scc/services/SimpleCsvConverter.java
+++ src/main/java/com/codecool/scc/services/SimpleCsvConverter.java
@@ -1,10 +1,13 @@
 package com.codecool.scc.services;
 
+import com.codecool.scc.dao.FileReader;
 import com.codecool.scc.view.OutputFormat;
+import com.codecool.scc.view.OutputFormatter;
 import com.codecool.scc.view.OutputFormatterFactory;
 
 import java.io.File;
-import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 
 public class SimpleCsvConverter {
 
@@ -16,11 +19,19 @@ public class SimpleCsvConverter {
         this.outputFormatterFactory = outputFormatterFactory;
     }
 
+    public void convert(File file) {
+        try {
+            List<String[]> data = fileReader.readData(file);
+            OutputFormatter outputFormatter = outputFormatterFactory.createByFormat(OutputFormat.TABLE);
+            outputFormatter.printToConsole(data);
+
+        } catch (IOException e) {
+            System.out.println("Wrong file name. Please check if such file exists.");
+        }
+    }
+
     public void convert(File file, OutputFormat outputFormat) {
         System.out.println("I convert CSV to output format");
     }
 
-    public void convert(File file) {
-        System.out.println("I convert CSV to output format");
-    }
 }
